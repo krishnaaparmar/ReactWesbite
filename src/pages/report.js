283ee7402b373a4f7b4dcb8725660547285df2cb
Krishna Parmar <98844700+krishnaaparmar@users.noreply.github.com>
2022-07-01 22:55:33 -0400

    Update report.js

--- src/pages/report.js
+++ src/pages/report.js
@@ -1,14 +1,68 @@
-import { useState } from "react";
-import cn from "classnames";
+
+import { useReducer } from "react";
+const initialTodos = [
+  {
+    id: 1,
+    title: "Todo 1",
+    complete: false,
+  },
+  {
+    id: 2,
+    title: "Todo 2",
+    complete: false,
+  },
+  {
+    id: 3,
+    title: "Todo 3",
+    complete: false,
+  },
+  {
+    id: 4,
+    title: "Todo 4",
+    complete: false,
+  }
+];
 
 
-export default function report() {
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "COMPLETE":
+      return state.map((todo) => {
+        if (todo.id === action.id) {
+          return { ...todo, complete: !todo.complete };
+        } else {
+          return todo;
+        }
+      });
+    default:
+      return state;
+  }
+};
+
+export default function Report() {
+  const [todos, dispatch] = useReducer(reducer, initialTodos);
+
+  const handleComplete = (todo) => {
+    dispatch({ type: "COMPLETE", id: todo.id });
+  };
+
   return (
     <>
-      
-      <div className="serviceImage"><br/>
-      <h1 className="animate__animated animate__slideInLeft">Services</h1>
+    <h4 style={{textAlign:"left", marginLeft:"8%"}}>Checklist using useReducer</h4>
+    <div className="bg reportResponsive" style={{width:"80%",marginLeft:"8%"}}>
+    
+      {todos.map((todo) => (
+        <div key={todo.id} style={{textAlign:"left", marginLeft:"5%"}}>
+         
+            <input
+              type="checkbox"
+              checked={todo.complete}
+              onChange={() => handleComplete(todo)}/>
+             <label style={{marginLeft:"10px"}}>{todo.title}
+          </label>
+        </div>
+      ))}
       </div>
-      </>
+    </>
   );
 }
