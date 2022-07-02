c1e6c311b15e955bd1a780ce6f5f852fdb50da98
Krishna Parmar <98844700+krishnaaparmar@users.noreply.github.com>
2022-07-01 22:56:32 -0400

    Update tools.js

--- src/pages/tools.js
+++ src/pages/tools.js
@@ -1,22 +1,24 @@
-import React, { useState, useEffect, useRef } from "react";
-import { Button } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
 import WeatherCard from "../components/weatherCard";
-import Todo from "../components/toDo";
-import Error from "../components/error";
+//mock data
+import Listdata from "../components/data.json";
+//components
+import ToDoList from "../components/ToDoList";
+import ToDoForm from '../components/ToDoForm';
+import Report from "./report";
 
 
 export default function Tools() {
-  const todoNameRef = useRef();
   const [data, setData] = useState([]);
-  const [toDoList, setToDos] = useState([]);
-  const [error, showError] = useState(false);
   const [weathError, showWeathError] = useState(false);
+  const [ toDoList, setToDoList ] = useState(Listdata);
 
   useEffect(() => {
     showWeathError(false);
     const fetchData = async () => {
-      await fetch(`https://api.openweathermap.org/data/2.5/weather/?lat=44.389355&lon=-79.690331&units=metric&APPID=92ad8d24c25849a4bd4e688a9a31031e`)
-        .then((res) => res.json())
+       await fetch(`https://api.openweathermap.org/data/2.5/weather/?lat=44.389355&lon=-79.690331&units=metric&APPID=6a07e24be765a5cefd9594f42f65d19e`)
+      //await fetch(`https://api.openweathermap.org/data/2.5/weather/?lat=44.389355&lon=-79.690331&units=metric&APPID=92ad8d24c25849a4bd4e688a9a31031e`)
+      .then((res) => res.json())
         .then((result) => {
           setData(result);
         });
@@ -24,60 +26,55 @@ export default function Tools() {
     fetchData();
   }, []);
 
-  const handleSubmit = () => {
-    const newValue = todoNameRef.current.value;
-    showError(false);
-    if (newValue === "") {
-      showError(true);
-      return;
-    }
-    setToDos([...toDoList, { text: newValue, completed: false }]);
-    todoNameRef.value = null;
-    document.getElementById("textInputField").value = "";
-  };
+  const handleToggle = (id) => {
+    let mapped = toDoList.map(task => {
+      return task.id === Number(id) ? { ...task, complete: !task.complete } : { ...task};
+    });
+    setToDoList(mapped);
+  }
 
-  const removeItem = (index) => {
-    const newToDoList = [...toDoList];
-    newToDoList.splice(index, 1);
-    setToDos(newToDoList);
-  };
+  const handleFilter = () => {
+    let filtered = toDoList.filter(task => {
+      return !task.complete;
+    });
+    setToDoList(filtered);
+  }
 
-  const toggleComplete = (index) => {
-    const newToDoList = [...toDoList];
-    newToDoList[index].completed = !newToDoList[index].completed;
-    setToDos(newToDoList);
-  };
+  const addTask = (userInput ) => {
+    let copy = [...toDoList];
+    copy = [...copy, { id: toDoList.length + 1, task: userInput, complete: false }];
+    setToDoList(copy);
+  }
+  
   return (
     <>
     <div className="bgImage1"><br/>
         <h1 className="animate__animated animate__slideInLeft">Tools</h1><br/><br/>
-        <div className="container pb-5">
-        <h4 className="font-weight-bold head4">Weather Application</h4>
-        <div className="align-items-center justify-content-center">
-          <WeatherCard weatherData={data} showError={showWeathError} error={weathError} />
-        </div>
-      </div>
+        <div className="container-fluid p-0">
+        <div className="row g-0">
+        <div className="col-lg-5 col-sm-6" style={{padding:"0 2%"}}>
+           <div className="container pb-5">
+            <h4 className="font-weight-bold head4">Weather Application</h4>
+             <div className="align-items-center justify-content-center">
+            <WeatherCard weatherData={data} showError={showWeathError} error={weathError} />
+            </div>
+            </div>
+            <Report/>
+         </div>
+          <div className="col-lg-7 col-sm-6 responsiveApp" style={{padding:"2%",width:"57%", border:"1px solid black"}}>
+          <h4 className="font-weight-bold head4">To-Do Application</h4>
+         
+            <ToDoList toDoList={toDoList} handleToggle={handleToggle} handleFilter={handleFilter}/>
+            <ToDoForm addTask={addTask}/>
 
-        
-      <div className="container py-5">
-        <h1 className="font-weight-bold header-animated py-4 head4">To-Do Application</h1>
-        {toDoList.length === 0 && <p>No to do task available</p>}
-        {toDoList.map((toDo, index) => (
-          <Todo key={index} toDo={toDo} index={index} toggleTodos={toggleComplete} removeToDo={removeItem} />
-        ))}
-        <div className="addInputItems">
-          <input id="textInputField" className="mx-4" ref={todoNameRef} type="text" />
-          <Button variant="primary" className="mx-2" onClick={handleSubmit}>
-            Add ToDo
-          </Button>
+          </div>
+          
         </div>
-        <Error showError={showError} error={error} />
-      </div>
-
+        </div>
 
         </div>
-     
 
+    
      
     </>
   );
